d1cb758d12879dcefa37df0a2a3a997bd2e6deeb
cokobah16-creator <cokobah16@gmail.com>
2025-09-30 08:38:17 +0100

    Fix Leaderboard database import

--- src/features/gamification/Leaderboard.tsx
+++ src/features/gamification/Leaderboard.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useGam } from '@/stores/gamification'
-import { db } from '@/db/mbhr'
+import { db } from '@/db'
 
 type Row = { volunteerId: string; tokens: number; name: string }
 
